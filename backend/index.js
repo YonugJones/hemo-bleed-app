60c66cbedfc386bff6bcc35f03d96487988268cc
Peter Kerfoot <peterckerfoot@gmail.com>
2025-05-20 15:07:40 -0500

    Added index entry point

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+
+// Define the main app
+const app = express()
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions))
+
+// Built in middleware for JSON
+app.use(express.json())
+
+// Built in Middleware to Handle Urlencoded Form Data
+app.use(express.urlencoded({ extended: true }))
+
+// middleware for cookies
+app.use(cookieParser())
+
+// Routes
+app.get('/', (req, res) => { res.json({ message: 'Welcome to the backend API for Hemophilia Bleed App' }) })
+
+// Global Error Handler
+app.use(errorHandler)
+
+// Start the server 
+const port = process.env.PORT || 3333
+app.listen(port, () => console.log(`App running at http://localhost:${port}`))
