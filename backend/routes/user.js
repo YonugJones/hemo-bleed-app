2acc03313529b09e2f4d3a3b614c692b0bac903a
Peter Kerfoot <peterckerfoot@gmail.com>
2025-05-23 17:31:20 -0500

    added route for editUser

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { getUser } = require('../controllers/user')
+const { getUser, editUser } = require('../controllers/user')
 const { authenticateToken } = require('../middleware/authMiddleware')
 const router = express.Router()
 
 router.get('/:username', authenticateToken, getUser)
+router.put('/:username', authenticateToken, editUser)
 
 module.exports = router
